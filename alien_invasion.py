b3309ca670797daf557a2625357bd4c08837215b
rajiv <rajiv3cha@gmail.com>
2019-11-10 23:49:28 -0500

    game has functional ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """overall class, used to manage assests/behaviours"""
@@ -25,22 +26,16 @@ class AlienInvasion:
         pygame.display.set_caption("Aliens")
         self.bg_colour = (230, 230, 230)
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """starts main loop for game"""
         while True: 
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
-    
-    def _update_screen(self):
-        """updates images on screen"""
-        # redraw screen each pass through the loop
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
-
-        # makes the most recently drawn screen visible
-        pygame.display.flip()
+            self._update_bullets()
     
     def _check_events(self):
         # event loop watches for KBM events. 
@@ -61,6 +56,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:        
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:        
+            self._fire_bullet()
         # quit the game
         elif event.key == pygame.K_q:
             sys.exit()
@@ -72,6 +69,28 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:        
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """create a new bullet object, add it to bullets group"""
+        if len(self.bullets) < self.settings.bullets_max:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_screen(self):
+        """updates images on screen"""
+        # redraw screen each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        # makes the most recently drawn screen visible
+        pygame.display.flip()
+
+    def _update_bullets(self):
+        """remove the bullets once they are off the screen"""
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
 if __name__ == '__main__':
     # make the game instance and run it
--- settings.py
+++ settings.py
@@ -6,3 +6,9 @@ class Settings:
         self.screen_height = 1080
         self.bg_color = (230,230,230)
         self.ship_speed = 1.5
+        # the bullets
+        self.bullet_speed = 1.0    
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = (100,0,0)
+        self.bullets_max = 10
